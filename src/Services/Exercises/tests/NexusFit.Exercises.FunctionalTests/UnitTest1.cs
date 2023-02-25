e71236dbbc1598026e45753ec7d37f0353eff045
Filipe Silva <filipeqs@outlook.com>
2023-02-24 21:40:47 -0500

    exercises tests projects added

---
+++ src/Services/Exercises/tests/NexusFit.Exercises.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace NexusFit.Exercises.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Services/Exercises/tests/NexusFit.Exercises.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace NexusFit.Exercises.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

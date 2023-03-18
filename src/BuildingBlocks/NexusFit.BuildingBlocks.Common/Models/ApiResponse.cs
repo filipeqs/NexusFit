4deee54abb5986ac5943f3e4a1b2600900031f34
Filipe Silva <filipeqs@outlook.com>
2023-03-18 19:15:05 -0400

    updated projects to .net 7

--- src/BuildingBlocks/NexusFit.BuildingBlocks.Common/Models/ApiResponse.cs
+++ src/BuildingBlocks/NexusFit.BuildingBlocks.Common/Models/ApiResponse.cs
@@ -15,11 +15,11 @@ public class ApiResponse
     {
         return statusCode switch
         {
-            400 => "A bad request, you have made",
-            401 => "Authorized, you are not",
-            404 => "Resource found, it was not",
-            500 => "Error are the path to the dark side. Errors lead to anger. Anger leads to hate. Hate leads to career change",
-            _ => null
+            400 => "Bad request",
+            401 => "You are not authorized",
+            404 => "Resource not found",
+            500 => "Server error",
+            _ => "Unidentified error occurred"
         };
     }
 }
